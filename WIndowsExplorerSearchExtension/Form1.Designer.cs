fa3d8e34d33b9386a6dcb15907c668d85c7b4993
Raghuveer Ch S <raghuveer57@users.noreply.github.com>
2024-04-19 22:22:11 +0530

    Add project files.

---
+++ WIndowsExplorerSearchExtension/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WIndowsExplorerSearchExtension
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(720, 12);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(306, 72);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(182, 12);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(306, 72);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(-1, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1243, 735);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 834);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ClearButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ WIndowsExplorerSearchExtension/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WIndowsExplorerSearchExtension
+{
+  
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Read the config file
+        /// Populate the list view with the file/folder names
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // search term
+            string searchTerm = "folder";
+
+            // TODO: API call, replace MockSearchApi with the actual API
+            List<string> searchResults = MockSearchApi(searchTerm);
+
+            // Update ListView with search results
+            listView1.Items.Clear();
+            foreach (var resultPath in searchResults)
+            {
+                listView1.Items.Add(new ListViewItem(resultPath));
+            }
+        }
+
+        private RootObject ReadConfig()
+        {
+            string configFilePath = Path.Combine(Application.StartupPath, "config.json");
+            string json = File.ReadAllText(configFilePath);
+            RootObject config = JsonConvert.DeserializeObject<RootObject>(json);
+            return config;
+        }
+
+
+        private List<string> MockSearchApi(string searchTerm)
+        {
+            // Read search results from the config.json file
+            RootObject config = ReadConfig();
+
+            List<string> filteredPaths = new List<string>();
+
+            // Assuming you want to return all paths if searchTerm is empty, or paths that contain the searchTerm otherwise
+            foreach (var result in config.SearchResults)
+            {
+                if (string.IsNullOrEmpty(searchTerm) || result.ResultPath.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    filteredPaths.Add(result.ResultPath);
+                }
+            }
+
+            return filteredPaths;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the search results and search term
+            listView1.Items.Clear();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            // Check if an item is selected
+            if (listView1.SelectedItems.Count == 1)
+            {
+                // Get the first selected item
+                ListViewItem item = listView1.SelectedItems[0];
+
+                // This will be the path to the file or folder, stored in the item's Text property.
+                string path = item.Text;
+
+                // Use the Process.Start method to open the file or folder
+                try
+                {
+                    System.Diagnostics.Process.Start(path);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Unable to open file or folder: {ex.Message}");
+                }
+            }
+        }
+    }
+    public class RootObject
+    {
+        public List<string> Drives { get; set; }
+        public List<SearchResult> SearchResults { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public string NodeType { get; set; }
+        public string ResultPath { get; set; }
+    }
+}
